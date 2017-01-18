580a740045a75d0f1b42c6c14f141654db8c502f
Filip Slatinac <fslat092@uottawa.ca>
2017-01-18 00:32:46 -0500

    big boi commit

---
+++ static/js/redirectAlert.js
@@ -0,0 +1,1 @@
+swal("In case you noticed","Hey, you might be wondering why you got redirected to a long and weird URL, that is because my web hosting service provider does not allow direct redirects. That will be changed as soon as my yearly contract ends.")

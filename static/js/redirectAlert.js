bcab6ad176095119c1f96092b9e9d737c070b487
Filip Slatinac <fslat092@uottawa.ca>
2017-01-18 00:41:10 -0500

    shit

--- static/js/redirectAlert.js
+++ static/js/redirectAlert.js
@@ -1,1 +1,2 @@
 swal("In case you noticed","Hey, you might be wondering why you got redirected to a long and weird URL, that is because my web hosting service provider does not allow direct redirects. That will be changed as soon as my yearly contract ends.")
+$(".sweet-alert").css('background-color', 'transparent');
